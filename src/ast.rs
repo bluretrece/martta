843a04bc410a673913aae4e2fd57f016d19ce3cc
bluretrece <luisvegasmor@gmail.com>
2022-01-20 22:49:22 -0400

    Effort of rewriting conways' syntax. Function calls sort of works and primitive values are supported as well.

--- src/ast.rs
+++ src/ast.rs
@@ -6,6 +6,7 @@ pub enum Prog {
 #[derive(Clone, Debug)]
 pub enum Stmt {
     Expr(Expr),
+    Assign(String, Expr),
 }
 
 #[derive(Clone, Debug)]
@@ -13,13 +14,23 @@ pub enum Expr {
     Int(i32),
     Bool(bool),
     Str(String),
+    Var(String),
+    Binary(Box<Expr>, Box<Expr>),
+    Call(Call),
 }
 
 #[derive(Clone, Debug)]
+pub struct Call {
+    pub func: String,
+    pub args: Vec<Box<Expr>>,
+}
+
+#[derive(Clone, Debug, std::cmp::PartialEq, PartialOrd)]
 pub enum Value {
     Int(i32),
     Bool(bool),
     Str(String),
+    Function(fn(Vec<Value>) -> Result<Value, String>),
     Nil,
 }
 
@@ -30,6 +41,7 @@ impl std::fmt::Display for Value {
             Self::Bool(b) => write!(f, "{}", *b),
             Self::Nil => write!(f, "Nil"),
             Self::Str(s) => write!(f, "{}", *s),
+            _ => unimplemented!(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod ast;
 use ast::*;
-
-use parser::ProgParser;
+use std::collections::HashMap;
 
 #[macro_use]
 extern crate lalrpop_util;
@@ -13,37 +12,113 @@ lalrpop_mod!(
     parser
 );
 
-fn eval(program: &Prog) -> Result<(), String> {
+#[derive(Debug, Default)]
+pub struct Environtment {
+    pub vals: HashMap<String, Value>,
+    // pub enclosing: Option<Rc<RefCell<Environment>>>,
+}
+
+impl Environtment {
+    pub fn define(&mut self, name: String, value: Value) -> Result<Value, String> {
+        self.vals.insert(name, value.clone());
+        Ok(value)
+    }
+
+    pub fn get_var(&mut self, name: String) -> Option<Value> {
+        self.vals.get(&name).map(|value| value.clone())
+    }
+}
+
+fn eval(program: &Prog, env: &mut Environtment) -> Result<(), String> {
     match program {
         Prog::Body { stmts } => {
             for stmt in stmts {
-                if let Ok(v) = stmt_eval(stmt) {
-                    println!("{:?}", v);
+                if let Err(err) = stmt_eval(stmt, env) {
+                    return Err(err);
                 }
             }
         }
     }
-
     Ok(())
 }
 
-fn stmt_eval(expr: &Stmt) -> Result<Value, String> {
+fn stmt_eval(expr: &Stmt, env: &mut Environtment) -> Result<Value, String> {
     match expr {
-        Stmt::Expr(x) => expr_eval(x),
-        _ => unimplemented!(),
+        Stmt::Expr(x) => expr_eval(x, env),
+        Stmt::Assign(name, rhs) => match expr_eval(rhs, env) {
+            Ok(v) => env.define(name.to_string(), v),
+            Err(e) => return Err(e),
+        },
     }
 }
 
-fn expr_eval(expr: &Expr) -> Result<Value, String> {
+fn expr_eval(expr: &Expr, env: &mut Environtment) -> Result<Value, String> {
     match expr {
+        Expr::Binary(lhs, rhs) => {
+            let lhs = expr_eval(lhs, env);
+            let rhs = expr_eval(rhs, env);
+
+            Ok(Value::Bool(lhs < rhs))
+        }
         Expr::Int(v) => Ok(Value::Int(*v)),
         Expr::Bool(b) => Ok(Value::Bool(*b)),
-        Expr::Str(s) => Ok(Value::Str(*s)),
-        _ => unimplemented!(),
+        Expr::Str(s) => Ok(Value::Str(s.to_string())),
+        Expr::Var(name) => match env.get_var(name.to_string()) {
+            Some(v) => Ok(v),
+            None => Err(format!("'{}' is not defined", name)),
+        },
+        Expr::Call(Call {
+            func: function,
+            args,
+        }) => {
+            let mut vals = Vec::new();
+
+            for arg in args {
+                match expr_eval(arg, env) {
+                    Ok(v) => vals.push(v),
+                    Err(e) => return Err(e),
+                }
+            }
+
+            let function_defined = match env.get_var(function.to_string()) {
+                Some(v) => v,
+                None => return Err(format!("Function '{}' is not defined", &function)),
+            };
+
+            if let Value::Function(f) = function_defined {
+                f(vals)
+            } else {
+                Err(format!("'{}' isn't a function", function))
+            }
+        }
     }
+}
+
+pub fn std_print(vals: Vec<Value>) -> Result<Value, String> {
+    println!("{:?}", vals);
+
+    Ok(Value::Nil)
 }
 
 fn main() {
-    let source = parser::ProgParser::new().parse("true;").unwrap();
-    println!("{:?}", eval(&source));
+    let mut env = Environtment::default();
+    env.define("print".to_string(), Value::Function(std_print));
+    let source = parser::ProgParser::new()
+        .parse("x = 5; print(x); print(12);")
+        .unwrap();
+    println!("{:?}", eval(&source, &mut env));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn basic_parsing() {
+        let mut env = HashMap::new();
+        let int_literal = "197;";
+        let source = parser::ProgParser::new().parse(int_literal).unwrap();
+        let res = eval(&source, &env);
+
+        assert_eq!(res, Ok(()));
+    }
 }
