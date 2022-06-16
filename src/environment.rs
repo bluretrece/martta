ff2046a7395715d00ed485307be46fc0d4e3c062
Luis Vegas <luisvegasmor@gmail.com>
2022-06-16 14:50:14 -0500

    The environment takes a reference to the parent env if needed

--- src/environment.rs
+++ src/environment.rs
@@ -3,7 +3,7 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Environment {
     pub vals: HashMap<String, Value>,
     pub enclosing: Option<Rc<RefCell<Environment>>>,
@@ -13,6 +13,13 @@ impl Environment {
     pub fn define(&mut self, name: String, value: Value) -> Result<(), String> {
         self.vals.insert(name, value);
         Ok(())
+    }
+
+    pub fn with_ref(environment: Rc<RefCell<Environment>>) -> Self {
+        Self {
+            vals: HashMap::new(),
+            enclosing: Some(environment),
+        }
     }
 
     pub fn get_var(&mut self, name: String) -> Option<Value> {
